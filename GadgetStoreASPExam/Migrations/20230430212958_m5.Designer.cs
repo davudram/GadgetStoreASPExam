c6a76539df37ffd9ef33e3345d60b22bc5bf32d0
Davud1Ramazanov <Rama_bz54@student.itstep.org>
2023-05-01 01:33:30 +0400

    A model was created for buying goods from the basket, as well as a controller for it.

--- GadgetStoreASPExam/Migrations/20230430212958_m5.Designer.cs
+++ GadgetStoreASPExam/Migrations/20230430212958_m5.Designer.cs
@@ -4,6 +4,7 @@ using GadgetStoreASPExam.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GadgetStoreASPExam.Migrations
 {
     [DbContext(typeof(DbContextClass))]
-    partial class DbContextClassModelSnapshot : ModelSnapshot
+    [Migration("20230430212958_m5")]
+    partial class m5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GadgetStoreASPExam/Model/BuyGadget.cs
@@ -0,0 +1,16 @@
+﻿namespace GadgetStoreASPExam.Model
+{
+    public class BuyGadget
+    {
+        public int Id { get; set; }
+        public string User { get; set; }
+        public int ProductId { get; set; }
+        public string NameCard { get; set; }
+        public int Month { get; set; }
+        public int Years { get; set; }
+        public int CVV { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
